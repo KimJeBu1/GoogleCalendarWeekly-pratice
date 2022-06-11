f91c123fc40cc7bbfc67b4880d1f7e7ecccde79e
KimJeBu1 <95723051+KimJeBu1@users.noreply.github.com>
2022-06-11 13:18:48 +0900

    Add contents in Calendar

--- src/pages/Calendar.tsx
+++ src/pages/Calendar.tsx
@@ -1,12 +1,22 @@
 import Header from '../components/Header'
+import SideCalendar from '../components/SideCalendar'
+import SideCalendarTitle from '../components/SideCalendarTitle'
 import { useSelector } from 'react-redux'
-import { useState } from 'react'
 import { currentCalendar } from '../store/modules/calendar'
-import { current } from '@reduxjs/toolkit'
+import ScheduleCalendar from '../components/ScheduleCalendar'
+import getThisWeek from '../util/getThisWeek'
+import { useState } from 'react'
+import AddScheduleButton from '../components/AddScheduleButton'
+import AddScheduleModal from '../components/AddScheduleModal'
+import formatDay from '../util/formatDay'
 
 export default function Calendar() {
   const { year, month, days } = useSelector(currentCalendar)
   const [isSideCalendar, setIsSideCalendar] = useState<boolean>(true)
+  const [isOpenModal, setIsOpenModal] = useState<boolean>(false)
+  const [isDeleteOpen, setIsDeleteOpe] = useState<boolean>(false)
+  const [modalDate, setModalDate] = useState<string>(formatDay(new Date()))
+  const [tiemIndex, setTimeIndex] = useState<number>(0)
 
   return (
     <>
@@ -16,6 +26,33 @@ export default function Calendar() {
         isSideCalendar={isSideCalendar}
         setIsSideCalendar={setIsSideCalendar}
       />
+      <main className="flex h-[calc(100%_-_3.5rem)] f;ex-1">
+        <AddScheduleButton
+          isSideCalendar={isSideCalendar}
+          isOpenModal={isOpenModal}
+          setIsOpenModal={setIsOpenModal}
+        />
+        <div className={`p-5 flex flex-col mt-[65px] ${isSideCalendar ? 'bolck' : 'hidden'}`}>
+          <SideCalendarTitle year={year} month={month} />
+          <SideCalendar days={days} />
+        </div>
+        <div className="flex felx-col h-full overflow-x-scroll flex-1 pr-2">
+          <ScheduleCalendar
+            days={getThisWeek(days)}
+            setModalDate={setModalDate}
+            setTimeIndex={setTimeIndex}
+            setIsOpenModal={setIsOpenModal}
+            isDeleteOpen={isDeleteOpen}
+            setIsDeleteOpen={setIsDeleteOpe}
+          />
+        </div>
+        <AddScheduleModal
+          defaultDate={modalDate}
+          timeIndex={tiemIndex}
+          isOpen={isOpenModal}
+          setIsOpen={setIsOpenModal}
+        />
+      </main>
     </>
   )
 }
